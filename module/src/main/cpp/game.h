94f0674ae870f35a3fac7c1bd1c69c3f18e2243e
yiqi3 <776799053@qq.com>
2025-06-28 04:55:06 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -1,10 +1,10 @@
 //
-// Created by Perfare on 2020/7/4.
+//由Perfare于2020年4月7日创建。
 //
 
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.game.packagename"
+#defineGamePackageName "com.herogame.gplay.lastdayrulessurvival"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
